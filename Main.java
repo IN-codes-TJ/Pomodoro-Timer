375dadc518aa0f24b76a6e68530b86da0f9c988e
INTJcodes <lolarosegooch@gmail.com>
2026-02-02 21:24:45 +0000

    Setting up the environment

---
+++ Main.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args); // Sets up our program as a javafx app and calls start
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
